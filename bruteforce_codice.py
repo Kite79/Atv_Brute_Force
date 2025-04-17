b20ccb0984d9fbe59289b764d7ea4aada23569fd
Kite79 <enricoalessi@hotmail.it>
2025-04-17 12:37:22 +0200

    aggiunto lunghezza automatica

--- bruteforce_codice.py
+++ bruteforce_codice.py
@@ -7,6 +7,7 @@ print("Benvenuto nella mia app!")
 # Input da console
 inizio = int(input("Inserisci il valore iniziale del range: "))
 fine = int(input("Inserisci il valore finale del range: "))
+lunghezza = len(str(fine))
 
 # Avviso iniziale per selezionare la finestra dove vuoi che venga eseguito lo script di brute force
 print("Hai 5 secondi per cliccare sulla finestra dove inserire il codice...")
@@ -14,8 +15,9 @@ time.sleep(5)  # ti dà il tempo di cliccare sulla finestra target
 
 # Inizia brute force dal valore che hai messo nelle variabili inizio e fine
 for i in range(inizio, fine):
-    codice = f"{i:04}"  # formatta con zeri: 0000, 0001, ..., 9999
+    codice = f"{i:0{lunghezza}d}"  # Formatta con zeri dinamicamente
     print(f"Inserisco codice: {codice}")
     pyautogui.write(codice)  # scrive il codice
     pyautogui.press("enter")  # preme invio
     time.sleep(0.5)  # attende 0.5 secondi prima del prossimo codice
+0
